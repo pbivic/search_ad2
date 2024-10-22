312392296974fc9f35ff6cf061bcd25ff5b10453
Bivic <paul.bivic@sekoia.io>
2024-10-22 16:36:50 +0200

    fixes

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from search_in_ad_modules import SearchInAdModule
 
 if __name__ == "__main__":
     module = SearchInAdModule()
-    module.register(SearchAction, "SearchAction")    
+    module.register(SearchAction, "search-ad")    
     module.run()
