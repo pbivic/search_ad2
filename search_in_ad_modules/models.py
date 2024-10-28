9921f0f9c133f5b813ef3c358d3d6cade9bea7f6
Bivic <paul.bivic@sekoia.io>
2024-10-28 15:45:21 +0100

    add args

--- search_in_ad_modules/models.py
+++ search_in_ad_modules/models.py
@@ -2,4 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class SearchInAdModuleConfiguration(BaseModel):
-    pass
+    servername: str = Field(..., description="Remote machine IP or Name")
+    admin_username: str = Field(..., description="Admin username")
+    admin_password: str = Field(secret=True, description="Admin password")
+
